db44bbdc6d0ddc8c069cb6e2d9fce7a593f85fab
sakun9526 <sakunrathnayaka@yahoo.com>
2022-10-03 08:39:50 +0530

    color container and buttons WIP

--- src/Components/Buttons.tsx
+++ src/Components/Buttons.tsx
@@ -1,5 +1,22 @@
+import Button from "@mui/material/Button";
+
 const Buttons = () => {
-  return <div>Buttons</div>;
+  return (
+    <>
+      <Button variant="contained" sx={{ ml: 60, mt: 5 }}>
+        Contained
+      </Button>
+      <Button variant="contained" sx={{ ml: 1.5, mt: 5 }}>
+        Contained
+      </Button>
+      <Button variant="contained" sx={{ ml: 1.5, mt: 5 }}>
+        Contained
+      </Button>
+      <Button variant="contained" sx={{ ml: 1.5, mt: 5 }}>
+        Contained
+      </Button>
+    </>
+  );
 };
 
 export default Buttons;
--- src/Components/ColorContainer.tsx
+++ src/Components/ColorContainer.tsx
@@ -1,5 +1,20 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
 const ColorContainer = () => {
-  return <div>ColorContainer</div>;
+  return (
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          width: 500,
+          height: 200,
+          backgroundColor: "#3B6078",
+          mt: 15,
+          borderRadius: 10,
+        }}
+      />
+    </Container>
+  );
 };
 
 export default ColorContainer;
