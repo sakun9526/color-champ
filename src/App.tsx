58064dddeca053a8b5b04b5e9b7c61fbb7e7ffa5
sakun9526 <sakunrathnayaka@yahoo.com>
2022-10-12 08:36:08 +0530

    added colors.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import "react-toastify/dist/ReactToastify.css";
-import Navbar from "./Components/Navbar";
-import ColorContainer from "./Components/ColorContainer";
-import Buttons from "./Components/Buttons";
-import ResultNotification from "./Components/ResultNotification";
-import Footer from "./Components/Footer";
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './Components/Navbar';
+import ColorContainer from './Components/ColorContainer';
+import Buttons from './Components/Buttons';
+import ResultNotification from './Components/ResultNotification';
+// import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar />
       <ColorContainer />
       <Buttons />
       <ResultNotification />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/Components/ColorContainer.tsx
+++ src/Components/ColorContainer.tsx
@@ -1,16 +1,23 @@
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Colors from './utils/Colors.json';
+import { useEffect } from 'react';
 
 const ColorContainer = () => {
+  useEffect(() => {
+    Colors.map((color) => {
+      console.log(color.color);
+    });
+  });
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth='sm'>
       <Box
         sx={{
           width: {
             sm: 550,
           },
           height: 200,
-          backgroundColor: "#3B6078",
+          backgroundColor: Colors[2].color,
           mt: 15,
           borderRadius: 10,
         }}
