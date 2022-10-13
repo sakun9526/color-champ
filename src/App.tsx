e837f0f4cb0a4ea6830c26993a17e3473610d74a
supuni9622 <supunimanamperi@gmail.com>
2022-10-13 09:26:18 +0530

    Added ColorContex

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Navbar from './Components/Navbar';
 import ColorContainer from './Components/ColorContainer';
 import Buttons from './Components/Buttons';
 import ResultNotification from './Components/ResultNotification';
+import { ColorContextProvider } from "./Components/Contexts/colorContext";
 // import Footer from './Components/Footer';
 
 function App() {
   return (
+    <ColorContextProvider>
     <div className='App'>
       <Navbar />
       <ColorContainer />
@@ -14,6 +16,7 @@ function App() {
       <ResultNotification />
       {/* <Footer /> */}
     </div>
+    </ColorContextProvider>
   );
 }
 
--- src/Components/ColorContainer.tsx
+++ src/Components/ColorContainer.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Colors from './utils/Colors.json';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ColorContainer = () => {
   useEffect(() => {
