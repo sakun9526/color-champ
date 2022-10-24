6b273b971ce692fe5560fcd2b0958814e31c091f
sakun9526 <sakunrathnayaka@yahoo.com>
2022-10-24 21:01:12 +0530

    random color generating method changed

--- src/Components/ColorContainer.tsx
+++ src/Components/ColorContainer.tsx
@@ -1,26 +1,38 @@
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
-import { useEffect } from "react";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import { useEffect, useState } from 'react';
+import { ColorsDigits } from './utils/ColorsDigits';
 
 const ColorContainer = () => {
+  const [color, setColor] = useState('');
+
   const colorGenerator = () => {
-    const color = Math.floor(Math.random() * 16777215).toString(16);
-    console.debug(color);
-    return `#${color}`;
+    const randomColor = new Array(6)
+      .fill('')
+      .map(() => {
+        return ColorsDigits[Math.floor(Math.random() * ColorsDigits.length)];
+      })
+      .join('');
+    console.log(randomColor);
+    return `#${randomColor}`;
   };
 
+  useEffect(() => {
+    setColor(colorGenerator());
+  }, []);
+
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth='sm'>
       <Box
         sx={{
           width: {
             sm: 550,
           },
           height: 200,
-          backgroundColor: colorGenerator(),
+          backgroundColor: color,
           mt: 15,
           borderRadius: 10,
-          borderColor: "black",
+          borderColor: 'black',
         }}
       />
     </Container>
